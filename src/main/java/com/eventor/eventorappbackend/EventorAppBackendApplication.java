f558fa2bd6a0a0b7c8395e42d6620d82235cf488
Igor Shmidt <schmidt.igor2012@yandex.ru>
2023-04-25 19:25:58 +0300

    initial commit

---
+++ src/main/java/com/eventor/eventorappbackend/EventorAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eventor.eventorappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventorAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventorAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventor/eventorappbackend/EventorAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventor.eventorappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventorAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
